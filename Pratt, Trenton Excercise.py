fa908451bb9ab6487ad295f507716267abef8c64
TrentonPratt <47069421+TrentonPratt@users.noreply.github.com>
2021-05-06 15:07:18 -0400

    Add files via upload

---
+++ Pratt, Trenton Excercise.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome()
+
+driver.get("https://www.google.com")
+
+search_bar = driver.find_element_by_name("q")
+search_bar.send_keys("RTS Labs")
+search_bar.send_keys(Keys.RETURN)
+
+result = driver.find_element_by_tag_name('h3')
+result.click()

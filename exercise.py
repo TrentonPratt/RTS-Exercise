52c4b3dde3ace38d884fcbab4c77bb18844be122
TrentonPratt <47069421+TrentonPratt@users.noreply.github.com>
2021-05-06 20:11:14 -0400

    Add files via upload

---
+++ exercise.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome()
+
+driver.get("https://www.google.com")
+
+search_bar = driver.find_element_by_name("q")
+search_bar.send_keys("RTS Labs")
+search_bar.send_keys(Keys.RETURN)
+
+result = driver.find_element_by_tag_name('h3') #first search result assumed to be first h3 element on page
+result.click()
+
+
